d171d8e5fc384a93040de11ac7fbc26d421a63c2
GrantChristie <grantchristie0@gmail.com>
2018-02-03 11:01:41 +0000

    heroku deployment changes

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # Setup database connections
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
