83305953a743e1036252309f6450ea7f90e8e231
GrantChristie <grantchristie0@gmail.com>
2018-02-04 19:51:02 +0000

    bootstrap added

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 db.init_app(app)
 migrate.init_app(app, db)
-
+bootstrap = Bootstrap(app)
 
 from app import routes, models
